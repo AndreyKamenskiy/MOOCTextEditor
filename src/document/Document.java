b43789d222a689a42cac9a7d1915953a5b971dfa
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2020-03-29 16:47:45 +0300

    delete comment

--- src/document/Document.java
+++ src/document/Document.java
@@ -133,6 +133,8 @@ public abstract class Document {
 	    // TODO: You will play with this method in week 1, and 
 		// then implement it in week 2
 	    return 0.456;
+	    
+	    //add some new comment  
 	}
 	
 	
